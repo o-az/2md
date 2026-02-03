9b8b7e5bba0df0833e0e9233801c427cb27d5356
o-az <omaraziz.dev@proton.me>
2026-02-02 20:50:18 -0800

    fix: use istanbul coverage instead of v8

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,9 @@ import { defineWorkersConfig } from '@cloudflare/vitest-pool-workers/config'
 export default defineWorkersConfig({
   test: {
     include: ['test/**/*.test.{ts,tsx}', 'test/**/*.spec.{ts,tsx}'],
+    coverage: {
+      provider: 'istanbul',
+    },
     poolOptions: {
       workers: {
         wrangler: { configPath: './wrangler.json' },
